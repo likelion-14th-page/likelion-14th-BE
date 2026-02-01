77fbc701948e636aa8ef0e34c0347435ccb1adbb
황신애 <shinae1023@naver.com>
2026-02-01 23:31:19 +0900

    [fix] Add HttpServletResponse import to SecurityConfig

--- src/main/java/com/likelion/hongik/global/config/SecurityConfig.java
+++ src/main/java/com/likelion/hongik/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @Configuration
