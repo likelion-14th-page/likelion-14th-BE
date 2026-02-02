c2807780201234f4f387b35758a87fbfb14ed84b
shinae <shinae1023@naver.com>
2026-02-02 22:16:39 +0900

    [feat] 홈컨트롤러 변경

--- src/main/java/com/likelion/hongik/HomeController.java
+++ src/main/java/com/likelion/hongik/HomeController.java
@@ -1,17 +1,17 @@
-package com.likelion.hongik;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.oas.annotations.Operation;
-
-@RequiredArgsConstructor
-@RestController
-public class HomeController {
-
-    @GetMapping("/")
-    @Operation(summary = "스웨거테스트")
-    public String home() {
-        return "멋쟁이사자처럼 14기 사이트 서버입니다.";
-    }
-}
+//package com.likelion.hongik;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import io.swagger.v3.oas.annotations.Operation;
+//
+//@RequiredArgsConstructor
+//@RestController
+//public class HomeController {
+//
+//    @GetMapping("/")
+//    @Operation(summary = "스웨거테스트")
+//    public String home() {
+//        return "멋쟁이사자처럼 14기 사이트 서버입니다.";
+//    }
+//}
