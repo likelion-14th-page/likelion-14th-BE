090334f3e50ac8f64f9525746c38f2e4aba9db2f
황신애 <shinae1023@naver.com>
2026-03-02 23:04:43 +0900

    Update additional document retrieval deadline

--- src/main/java/com/likelion/hongik/service/StudentResultService.java
+++ src/main/java/com/likelion/hongik/service/StudentResultService.java
@@ -28,7 +28,7 @@ public class StudentResultService {
                 .orElseThrow(() -> new IllegalArgumentException("지원자 정보를 찾을 수 없습니다."));
 
         // 추가 지원자 서류 결과 조회 시간 예외 처리
-        LocalDateTime additionalTargetDoc = LocalDateTime.of(2026,3,2,10,0);
+        LocalDateTime additionalTargetDoc = LocalDateTime.of(2026,3,3,10,0);
         if (Boolean.TRUE.equals(studentResult.getStudent().getAddition())
                                && LocalDateTime.now().isBefore(additionalTargetDoc)) {
             throw new IllegalArgumentException("아직 추가 지원 서류 결과 조회 시간이 아닙니다.");
